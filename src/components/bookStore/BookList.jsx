285d6c47c158ef455892132e6115aca6332a9654
numangit <abdulnumanjob@gmail.com>
2023-03-02 18:35:36 +0600

    all and feature button toggle added

--- src/components/bookStore/BookList.jsx
+++ src/components/bookStore/BookList.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookCard from "./BookCard";
 
 const BookList = () => {
+
+    const [active, setActive] = useState("All");
+
+    //filter handler function
+    const filterHandler = (selectedFiler) => {
+        if (selectedFiler === "Featured") {
+            setActive("Featured");
+        } else {
+            setActive("All");
+        }
+    };
 
     return (
         <div class="order-2 xl:-order-1">
@@ -10,8 +21,21 @@ const BookList = () => {
                 <h4 class="mt-2 text-xl font-bold">Book List</h4>
 
                 <div class="flex items-center space-x-4">
-                    <button class="filter-btn active-filter" id="lws-filterAll">All</button>
-                    <button class="filter-btn" id="lws-filterFeatured">Featured</button>
+                    <button
+                        class={`filter-btn ${active === "All" && "active-filter"}`}
+                        id="lws-filterAll"
+                        onClick={() => filterHandler("All")}
+                    >
+                        All
+                    </button>
+
+                    <button
+                        class={`filter-btn ${active === "Featured" && "active-filter"}`}
+                        id="lws-filterFeatured"
+                        onClick={() => filterHandler("Featured")}
+                    >
+                        Featured
+                    </button>
                 </div>
             </div>
 
