41b11b540330ce3a4b71d82de3379eb80ec7b350
numangit <abdulnumanjob@gmail.com>
2023-03-02 18:08:29 +0600

    books action types added

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ import Navbar from './components/navbar/Navbar'
 function App() {
   return (
     <div>
+
       <Navbar />
+
       <main class="py-12 2xl:px-6">
         <div class="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+
           <BookList />
+
           <AddBook />
+
         </div>
       </main>
     </div>
---
+++ src/redux/booksFeatures/actionTypes.jsx
@@ -0,0 +1,4 @@
+export const FETCHBOOK = "books/fetchBook";
+export const ADDBOOK = "books/addBook";
+export const UPDATEBOOK = "books/updateBook";
+export const DELETEBOOK = "books/deleteBook";
